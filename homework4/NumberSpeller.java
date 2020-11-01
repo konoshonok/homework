0b313d7572c396194a75ec72489419071c98942e
konoshonok <konoshonok9@gmail.com>
2020-11-01 04:17:01 +0300

    добавлена пропись даблов

--- homework4/NumberSpeller.java
+++ homework4/NumberSpeller.java
@@ -1,5 +1,8 @@
 package homework4;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Преобразователь числа в пропись
  */
@@ -11,10 +14,12 @@ public class NumberSpeller {
                                                                 {"", "тысяч ", "миллионов "}};
     private static final String[] textHundreds = {"", "сто ", "двести ", "триста ", "четыреста ", "пятьсот ", "шестьсот ", "семьсот ", "восемьсот ", "девятьсот "};
     private static final String[] textTens = {"", "десять ", "двадцать ", "тридцать ", "сорок ", "пятьдесят ", "шестьдесят ", "семьдесят ", "восемьдесят ", "девяносто "};
-    private static final String[] textFrom10To19 = {"десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семьнадцать", "восемнадцать", "девятнадцать"};
+    private static final String[] textFrom10To19 = {"десять ", "одиннадцать ", "двенадцать ", "тринадцать ", "четырнадцать ", "пятнадцать ", "шестнадцать ", "семьнадцать ", "восемнадцать ", "девятнадцать "};
     private static final String[] textUnits = {"", "од", "дв", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+    private static final String[][] textFractions = {{"целая ", "сотая"}, {"целых ", "сотых"}};
 
     private static int number;                       // Число, которое нужно преобразовать
+    private static double numberDbl;                 // Число, которое нужно преобразовать
     private static int numberMax = 999_999_999;
     private static int millions;
     private static int thousands;
@@ -23,12 +28,17 @@ public class NumberSpeller {
     private static int tens;
     private static int units;
     private static int indexN;                       // Индекс строки массива "textMillionsAndThousands"
+    private static int indexFirst;                   // Индекс строки массива "textFractions"
+    private static int indexSecond;                  // Индекс строки массива "textFractions"
+    private static int firstPart;                    // Целая часть дробного числа
+    private static int secondPart;                   // Дробная часть (остаток от деления) дробного числа
 
-    private static String spelledNum;                // Число прописью
+    private static String spelledNum;                // Целое число прописью
+    private static String spelledNumDbl;             // Дробное число прописью
 
 
     /**
-     * Метод, который преобразует число в пропись
+     * Метод, который преобразует целое число в пропись
      * @param n число, которое нужно преобразовать
      * @return число прописью
      */
@@ -46,7 +56,7 @@ public class NumberSpeller {
         }
 
         if (number == 0 ) {
-            return "ноль";
+            return "ноль ";
         }
 
         // Деление числа на степени и разряды
@@ -115,5 +125,43 @@ public class NumberSpeller {
         spelledNum = spelledNum + textMillionsAndThousands[indexN][index];
 
         return spelledNum;
+    }
+
+    /**
+     * Метод, который преобразует дробное число в пропись
+     * @param n число, которое нужно преобразовать
+     * @return число прописью
+     */
+    public static String spellIt (double n) {
+        numberDbl = n;
+
+        firstPart = (int) n;
+
+        BigDecimal bigN = new BigDecimal(n);
+        bigN = bigN.setScale(2, RoundingMode.DOWN);
+
+        BigDecimal bigHundred = new BigDecimal(100.00);
+
+        secondPart = (bigN.multiply(bigHundred).remainder(bigHundred)).intValueExact();
+
+        // Формирование окончаний слов "целая", "целых"
+        indexFirst = 0;
+        if (firstPart % 10 == 1) {
+            indexFirst = 0;
+        } else {
+            indexFirst = 1;
+        }
+
+        // Формирование окончаний слов "сотая", "сотых"
+        indexSecond = 0;
+        if (secondPart % 10 == 1) {
+            indexSecond = 0;
+        } else {
+            indexSecond = 1;
+        }
+
+        spelledNumDbl = spellIt(firstPart) + textFractions[indexFirst][0] + spellIt(Math.abs(secondPart)) + textFractions[indexSecond][1];
+
+        return spelledNumDbl;
     }
 }
--- homework4/NumberSpellerMain.java
+++ homework4/NumberSpellerMain.java
@@ -5,22 +5,45 @@ import java.util.Scanner;
 
 public class NumberSpellerMain {
     public static void main(String[] args) {
-        int number;
+        int input;
+        int numberInt;
+        double numberDbl;
 
         Scanner in = new Scanner(System.in);
 
-        try {
-            do {
-                System.out.print("Введите целое число от -999_999_999 до +999_999_999 (для выхода введите 0): ");
+        do {
+            System.out.print("Выберите тип числа: 1 - целое; 2 - дробное (для выхода введите 0): ");
 
-                number = in.nextInt();
+            input = in.nextInt();
 
-                System.out.println(NumberSpeller.spellIt(number));
-            } while (number != 0);
+            switch (input) {
+                case 1:
+                    try {
+                        do {
+                            System.out.print("Введите целое число от -999_999_999 до +999_999_999 (для выхода в меню введите 0): ");
+                            numberInt = in.nextInt();
+                            System.out.println(NumberSpeller.spellIt(numberInt));
+                        } while (numberInt != 0);
+                    } catch (InputMismatchException e) {
+                        System.out.println("Недопустимое значение");
+                    }
+                    break;
 
-            System.out.println("Был рад помочь! До свидания!");
-        } catch (InputMismatchException e) {
-            System.out.println("Недопустимое значение");
-        }
+                case 2:
+                    try {
+                        do {
+                            System.out.print("Введите дробное число от -999_999_999 до +999_999_999 (для выхода в меню введите 0): ");
+                            numberDbl = in.nextDouble();
+                            System.out.println(NumberSpeller.spellIt(numberDbl));
+                        } while (numberDbl != 0);
+                    } catch (InputMismatchException e) {
+                        System.out.println("Недопустимое значение");
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        } while (input != 0);
     }
 }
